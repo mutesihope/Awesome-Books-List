17c3618b23e01dd9aba86c8cf993e33ae4181708
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-08-08 15:20:03 +0500

    Updated index.js file to remoce hr line

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-  // Function to remove a book from the list and update local storage
+  // // Function to remove a book from the list and update local storage
+  // function removeBook(event) {
+  //   const bookDiv = event.target.parentNode;
+  //   bookDiv.remove();
+    
+  
+  //   // Update local storage after removing the book
+  //   updateLocalStorage();
+  // }
+  
   function removeBook(event) {
     const bookDiv = event.target.parentNode;
+    const hrLine = bookDiv.nextElementSibling; // Get the <hr> element after the book div
     bookDiv.remove();
+    if (hrLine) {
+      hrLine.remove(); // Remove the <hr> line if it exists
+    }
   
     // Update local storage after removing the book
     updateLocalStorage();
