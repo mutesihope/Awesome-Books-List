c1081276903b823ec94c88afc6338f5ac8c80afc
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-08-08 09:22:07 +0500

    removed errors from Index.js local storage issue

--- index.js
+++ index.js
@@ -1,53 +1,105 @@
+  // Function to remove a book from the list and update local storage
+  function removeBook(event) {
+    const bookDiv = event.target.parentNode;
+    bookDiv.remove();
+  
+    // Update local storage after removing the book
+    updateLocalStorage();
+  }
 
-const books = [];
-function addBook(title, author) {
-  const book = {
-    title,
-    author,
-  };
-  books.push(book);
+// Function to add a book to the list and update local storage
+function addBook(event) {
+  event.preventDefault();
 
-  // Save the books in localStorage
-localStorage.setItem('books', JSON.stringify(books));
+  const titleInput = document.getElementById('title');
+  const authorInput = document.getElementById('author');
+  const title = titleInput.value;
+  const author = authorInput.value;
 
-  // Display the new book in the page
-  const bookList = document.querySelector(".Booklist");
-  const li = document.createElement("li");
-  li.textContent = `${book.title}<span> by </span>${book.author}`;
-  bookList.appendChild(li);
+  // avoids default or empty submition 
+    if (title.trim() === '' || author.trim() === '') {
+    alert('Please enter both title and author name.');
+    return;
+  }
+
+
+  const bookList = document.querySelector('.Booklist ul');
+  const newBookItem = document.createElement('li');
+  newBookItem.innerHTML = `${title}<span> by </span>${author}`;
+
+  const removeButton = document.createElement('button');
+  removeButton.className = 'button-remove';
+  removeButton.textContent = 'Remove';
+  removeButton.addEventListener('click', removeBook);
+
+  // adds a new book and remove button
+  const bookDiv = document.createElement('div');
+  bookDiv.className = 'Booklist';
+  bookDiv.appendChild(newBookItem);
+  bookDiv.appendChild(removeButton);
+
+  const hrLine = document.createElement('hr');
+  hrLine.className = 'hr-line';
+
+  bookList.appendChild(bookDiv);
+  bookList.appendChild(hrLine);
+
+  // Clear input fields after adding the book
+  titleInput.value = '';
+  authorInput.value = '';
+
+  // Update local storage after adding the book
+  updateLocalStorage();
 }
 
+// Function to update local storage with the current book list
+function updateLocalStorage() {
+  const bookList = document.querySelectorAll('.Booklist ul li');
+  const books = [];
 
- // Function to remove a book from the collection
-function removeBook(index) {
-  books = books.filter((book, i) => i !== index);
+  bookList.forEach((bookItem) => {
+    const title = bookItem.innerText.split(' by ')[0];
+    const author = bookItem.innerText.split(' by ')[1];
+    books.push({ title, author });
+  });
 
-  // Save the books in localStorage
-  localStorage.setItem("books", JSON.stringify(books));
-
-  // Remove the book from the page
-  const bookList = document.querySelector(".Booklist");
-  const li = bookList.children[index];
-  li.remove();
+  localStorage.setItem('books', JSON.stringify(books));
 }
 
-// Load the books from localStorage
-if (localStorage.getItem("books")) {
-  books = JSON.parse(localStorage.getItem("books")) || [];
+// Function to load books from local storage
+function loadBooks() {
+  const books = JSON.parse(localStorage.getItem('books'));
+
+  if (books) {
+    const bookList = document.querySelector('.Booklist ul');
+    bookList.innerHTML = '';
+
+    books.forEach((book) => {
+      const newBookItem = document.createElement('li');
+      newBookItem.innerHTML = `${book.title}<span> by </span>${book.author}`;
+
+      const removeButton = document.createElement('button');
+      removeButton.className = 'button-remove';
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', removeBook);
+
+      const bookDiv = document.createElement('div');
+      bookDiv.className = 'Booklist';
+      bookDiv.appendChild(newBookItem);
+      bookDiv.appendChild(removeButton);
+
+      const hrLine = document.createElement('hr');
+      hrLine.className = 'hr-line';
+
+      bookList.appendChild(bookDiv);
+      bookList.appendChild(hrLine);
+    });
+  }
 }
 
-// Add event listeners to the buttons
-const addButton = document.querySelector(".button-submit");
-addButton.addEventListener("click", () => {
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  addBook(title, author);
-});
+// Add event listener to the Add button
+const addButton = document.querySelector('.button-submit');
+addButton.addEventListener('click', addBook);
 
-const removeButton = document.querySelector(".button-remove");
-removeButton.addEventListener("click", () => {
-  const bookList = document.querySelector(".Booklist");
-  const li = bookList.children[bookList.children.length - 1];
-  const index = bookList.children.indexOf(li);
-  removeBook(index);
-});
+// Load books from local storage on page load
+window.addEventListener('load', loadBooks);
